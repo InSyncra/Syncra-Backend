709a1510e8e1fbb991e845d7d7330e3de40fc4d3
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-07-19 20:49:50 -0400

    update firebase config

--- utils/lib/firebase.config.js
+++ utils/lib/firebase.config.js
@@ -2,20 +2,22 @@
  * Firebase configuration to connect to the backend
  * DO NOT MODIFY THIS FILE IN ANY WAY
  *
- * TODO: To get with Imani about Firebase Project Setup
+ * 
  */
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: "devmate-test",
+    projectId: "devmate-6d52a",
     storageBucket: process.env.FIREBASE_STORAGE_URL,
     messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
     appId: process.env.FIREBASE_APP_ID
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
