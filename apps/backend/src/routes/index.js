56bd0abf94cc185780ea75e9fa24f44db6952abf
Isaiah Vickers <isaiah.vickers@outlook.com>
2024-12-17 05:31:10 -0500

    update prisma client

--- apps/backend/src/routes/index.js
+++ apps/backend/src/routes/index.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import { prisma } from "@repo/db";
 
 const routes = Router();
+
+routes.get("/", async (req, res) => {
+    res.send(
+        `<h1>Welcome to Syncra backend!</h1> <p>This project is designed for authorized users to clone and access the codebase. Instructions will be posted soon.</p>`,
+    );
+});
 
 export default routes;
