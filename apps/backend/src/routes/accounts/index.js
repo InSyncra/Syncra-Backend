f9f372b6835044be44641bbef3623ebd7774e7c1
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-03-27 05:56:24 -0400

    Refactor(auth): Enhance User Session Restoration
    - Updated user session restoration to use findUniqueOrThrow for better error handling.
    - Created ReqUserObject class to encapsulate user information (id and email).
    - Added logging for user requests to track user activity.

--- apps/backend/src/routes/accounts/index.js
+++ apps/backend/src/routes/accounts/index.js
@@ -1,13 +1,5 @@
 import { Router } from "express";
-import {
-	deleteUserById,
-	getAllUsers,
-	getUserById,
-	login,
-	logout,
-	signup,
-	updateUserById,
-} from "../../controllers/accountsControllers.js";
+import { deleteUserById, getAllUsers, getUserById, updateUserById } from "../../controllers/accountsControllers.js";
 import { requireAuth } from "../../utils/auth.js";
 
 const userRoutes = Router();
@@ -15,20 +7,11 @@ const userRoutes = Router();
 // get all user accounts
 userRoutes.get("/", getAllUsers);
 
-// create a new user and authenticate them
-userRoutes.post("/signup", signup);
-
-// login a user
-userRoutes.post("/login", login);
-
-// get user account
+// get user account by id
 userRoutes.get("/:id", getUserById);
 
 // update user account
 userRoutes.put("/:id", requireAuth, updateUserById);
-
-// log user out (delete user session, not entire account)
-userRoutes.delete("/", logout);
 
 // delete user account
 userRoutes.delete("/:id", requireAuth, deleteUserById);
