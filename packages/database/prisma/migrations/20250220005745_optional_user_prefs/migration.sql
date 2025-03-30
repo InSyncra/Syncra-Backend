73f9019a92c4acd024577b7b8a3b4e90995726fd
Isaiah Vickers <140284664+izzymadethat@users.noreply.github.com>
2025-03-29 23:48:09 -0400

    Update/Operation Move To Vercel (#38)
    * refactor(config): Remove Unused Configuration Files
    
    - Deleted .prettierignore, .prettierrc, eslint.config.js, jsconfig.json, package-lock.json, pnpm-lock.yaml, pnpm-workspace.yaml, turbo.json, Dockerfile, and git-guidelines.md as they are no longer needed.
    - Cleaned up the project structure by removing obsolete files to streamline the development process.
    - Ensured that the codebase is more maintainable and focused on current configurations.
    
    * Refactor(package): Consolidate Package Configuration and Remove Redundant Files
    
    - Updated package.json to streamline dependencies and scripts for the backend setup.
    - Removed obsolete biome.json and README.md files from the backend directory to declutter the project structure.
    - Added nodemon as a development dependency for improved development experience.
    - Consolidated Docker-related configurations by removing unnecessary files.
    
    * chore: clean up existing monorepo files & integrate into one repo
    
    * chore(pre-committing): add commitlinting and git commit automation
    
    * ci: improve dependabot and other ci settings
    
    target dependabot to work on dev and merge automatically. delete docker ci as it's no longer needed.
    
    * chore: add scripts to have more efficient dev workflow

--- packages/database/prisma/migrations/20250220005745_optional_user_prefs/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "User" DROP CONSTRAINT "User_userPrefsId_fkey";
-
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "userPrefsId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "User" ADD CONSTRAINT "User_userPrefsId_fkey" FOREIGN KEY ("userPrefsId") REFERENCES "UserPrefs"("id") ON DELETE SET NULL ON UPDATE CASCADE;
---
+++ src/controllers/authControllers.js
@@ -0,0 +1,101 @@
+import bcrypt from "bcryptjs";
+import { generateJWT } from "../utils/auth.js";
+import { prisma } from "../utils/prisma.js";
+import { validateRequestBody } from "../utils/validations/zod-error-formatter.js";
+import { userCredentialSchema, userSchema } from "../utils/validations/zod-schemas.js";
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Function} next
+ * @returns
+ */
+export async function signup(req, res, next) {
+	validateRequestBody(userSchema, req, next);
+
+	// try catch block to handle errors
+	try {
+		// remove the password from the request body by destructuring it
+		const { password, ...userData } = req.body;
+
+		// hash the password using bcrypt
+		const hashedPassword = await bcrypt.hash(password, 10);
+
+		// create a new user in the database
+		const user = await prisma.user.create({
+			data: {
+				...userData,
+				hashedPassword,
+			},
+		});
+
+		// Generate token after creation so that user
+		// doesn't have to sign in again
+		// Set req.user to newly created user
+		generateJWT(res, user);
+
+		// send the user data as a response
+		res.status(201).json(user);
+	} catch (error) {
+		// catch any errors and send a 500 status code with an error message
+		next(error);
+	}
+}
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {Function} next
+ * @returns
+ */
+export async function login(req, res, next) {
+	validateRequestBody(userCredentialSchema, req, next);
+	const { credential, password } = req.body;
+
+	try {
+		const user = await prisma.user.findFirst({
+			where: {
+				OR: [{ email: credential }, { username: credential }],
+			},
+			select: {
+				email: true,
+				id: true,
+				hashedPassword: true,
+			},
+		});
+
+		if (!user) {
+			const error = new Error("Invalid credentials");
+			error.title = "Login Failed";
+			error.status = 401;
+			return next(error);
+		}
+
+		const isPasswordMatch = await bcrypt.compare(password, user.hashedPassword);
+
+		if (!isPasswordMatch) {
+			return res.status(401).json({ message: "Invalid credentials" });
+		}
+
+		// Generate token after sign in so that user can use Syncra
+		const { hashedPassword, ...userPayload } = user;
+		generateJWT(res, userPayload);
+
+		return res.status(200).json({ message: "User logged in successfully" });
+	} catch (error) {
+		next(error);
+	}
+}
+
+/**
+ * Clears authentication token from user
+ * @param {Request} _req
+ * @param {Response} res
+ * @returns
+ */
+export async function logout(_req, res) {
+	res.clearCookie("token");
+	return res.status(200).json({ message: "User logged out successfully" });
+}
