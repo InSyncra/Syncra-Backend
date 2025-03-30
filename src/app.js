438bddbf6237c42401f0d410d002c6878b031655
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-03-30 01:42:32 -0400

    build(deployment): move vercel to deployment successfully
    remove files and folders that vercel doesn't need.
    
    BREAKING CHANGE: Removes the bin/www file which may cause conflict error

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ import "dotenv/config";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
+import config from "../config/index.js";
+const { port, environment } = config;
 
 import routes from "./routes/index.js";
 
@@ -32,5 +34,21 @@ app.get("/", async (req, res) => {
 
 // Connect all the routes
 app.use("/api/v1", routes);
+
+app.listen(port, () => {
+	if (environment === "development") {
+		console.log("\n\n***********************************");
+		console.log("***********************************");
+		console.log(`Server is now listening on port ${port}. Test it out on http://localhost:${port}/`);
+		console.log("***********************************");
+		console.log("***********************************\n\n");
+	} else {
+		console.log("\n\n***********************************");
+		console.log("***********************************");
+		console.log(`Server is now listening on port ${port}.`);
+		console.log("***********************************");
+		console.log("***********************************\n\n");
+	}
+});
 
 export default app;
