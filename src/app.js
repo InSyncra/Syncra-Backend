5f7f35736428f1c344e7fda9c570a29517e70077
Isaiah Vickers <isaiah.vickers@outlook.com>
2025-04-21 07:32:59 -0400

    fix(cors): add CORS allowed origins to only the frontend routes

--- src/app.js
+++ src/app.js
@@ -17,7 +17,20 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
+const allowedOrigins = ["https://syncra-frontend.vercel.app", "http://localhost:5173", "http://127.0.0.1:5173"];
+
+const corsOptions = {
+	origin: (origin, cb) => {
+		if (!origin) return cb(null, true); // for mobile apps/curl req
+		if (!allowedOrigins.includes(origin)) {
+			return cb(new Error("Not allowed by CORS"));
+		}
+		return cb(null, true);
+	},
+	credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(
 	helmet.crossOriginResourcePolicy({
