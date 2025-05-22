a9e9df8c6ea2f62058d364d4626e4ba191d4682c
Ruben Cuello <kuelmusic@gmail.com>
2025-05-22 14:30:05 -0400

    fixed rework kinks

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,12 +7,12 @@ import projectRoutes from "./projects/index.js";
 import router from "./webhook/stripe.js";
 const routes = express.Router();
 
-/* 
+/*
 	TO RUBEN: When you add your webhooks, add them before the express.json() middleware.
 
 	Stripe requires the raw json format, and express.json() parses to an actual JS Object, which for some reason errors out when both middlewares are used.
 */
-
+routes.use("/webhook/stripe", router);
 routes.use(express.json());
 routes.use(express.urlencoded({ extended: true }));
 
